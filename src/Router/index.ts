e5829814764221c448e0e49dd584201fe07f4150
Dominik Zogg <dominik.zogg@gmail.com>
2020-04-24 10:26:44 +0200

    one route per line

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -5,48 +5,12 @@ import Home from '../Component/Page/Home.vue';
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-    {
-        path: '/',
-        component: Home,
-        meta: {
-            title: 'Home',
-        }
-    },
-    {
-        path: '/pet',
-        component: () => import('../Component/Page/Pet/List.vue'),
-        meta: {
-            title: 'List Pets',
-        }
-    },
-    {
-        path: '/pet/create',
-        component: () => import('../Component/Page/Pet/Create.vue'),
-        meta: {
-            title: 'Create Pet',
-        }
-    },
-    {
-        path: '/pet/:id',
-        component: () => import('../Component/Page/Pet/Read.vue'),
-        meta: {
-            title: 'Read Pet',
-        }
-    },
-    {
-        path: '/pet/:id/update',
-        component: () => import('../Component/Page/Pet/Update.vue'),
-        meta: {
-            title: 'Update Pet',
-        }
-    },
-    {
-        path: '*',
-        component: () => import('../Component/Page/NotFound.vue'),
-        meta: {
-            title: 'Not Found',
-        }
-    }
+    { path: '/', component: Home, meta: { title: 'Home' } },
+    { path: '/pet', component: () => import('../Component/Page/Pet/List.vue'), meta: { title: 'List Pets' } },
+    { path: '/pet/create', component: () => import('../Component/Page/Pet/Create.vue'), meta: { title: 'Create Pet' } },
+    { path: '/pet/:id', component: () => import('../Component/Page/Pet/Read.vue'), meta: { title: 'Read Pet' } },
+    { path: '/pet/:id/update', component: () => import('../Component/Page/Pet/Update.vue'), meta: { title: 'Update Pet' } },
+    { path: '*', component: () => import('../Component/Page/NotFound.vue'), meta: { title: 'Not Found' } },
 ];
 
 const router = new VueRouter({
